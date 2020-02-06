6a5695d3c2ea22de18c393b7bc2c6637bcec9d93
jesseleeruiz <jesseleeruiz@gmail.com>
2020-02-05 20:42:37 -1000

    Added GFTitleLabel file to reuse the same font and settings for title labels throughout the app.

---
+++ GitHubFollowers/GitHubFollowers/GFTitleLabel.swift
@@ -0,0 +1,36 @@
+//
+//  GFTitleLabel.swift
+//  GitHubFollowers
+//
+//  Created by Jesse Ruiz on 2/5/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
