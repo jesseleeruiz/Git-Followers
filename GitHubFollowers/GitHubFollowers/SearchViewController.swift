17d6dced1ad7b18daa8619ac7ff6256392c4ec61
jesseleeruiz <jesseleeruiz@gmail.com>
2020-02-05 21:19:23 -1000

    Added Alert View Controler to configure alert controller anywhere in the app. Created extension on UIViewController to present the alert in customizable way on all VC.

--- GitHubFollowers/GitHubFollowers/SearchViewController.swift
+++ GitHubFollowers/GitHubFollowers/SearchViewController.swift
@@ -37,7 +37,7 @@ class SearchViewController: UIViewController {
     // MARK: Methods
     @objc func pushFollowerListVC() {
         guard isUserNameEntered else {
-            print("No username")
+            presentGFAlertOnMainThread(title: "Empty username", message: "Please enter a username. We need to know who to look for. 😃", buttonTitle: "Ok")
             return
         }
         
---
+++ GitHubFollowers/GitHubFollowers/UIViewController+EXT.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+EXT.swift
+//  GitHubFollowers
+//
+//  Created by Jesse Ruiz on 2/5/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertViewController(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
