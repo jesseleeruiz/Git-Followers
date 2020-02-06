59772697fc4bd0d5becbd094ecea68d7613cbf3c
jesseleeruiz <jesseleeruiz@gmail.com>
2020-02-05 15:51:07 -1000

    Added Button File and TextField File to project.

---
+++ GitHubFollowers/GitHubFollowers/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Jesse Ruiz on 2/5/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
