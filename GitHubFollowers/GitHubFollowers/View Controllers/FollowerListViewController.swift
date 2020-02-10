689e711233263ede29400d3fea8d6a0cdcaea2c6
jesseleeruiz <jesseleeruiz@gmail.com>
2020-02-09 19:51:27 -1000

    Added empty state view when user does not have any followers or favorites.

--- GitHubFollowers/GitHubFollowers/View Controllers/FollowerListViewController.swift
+++ GitHubFollowers/GitHubFollowers/View Controllers/FollowerListViewController.swift
@@ -64,6 +64,14 @@ class FollowerListViewController: UIViewController {
             case .success(let followers):
                 if followers.count < 100 { self.hasMoreFollowers = false }
                 self.followers.append(contentsOf: followers)
+                
+                if self.followers.isEmpty {
+                    let message = "This user does not have any followers. Go follow them 😃."
+                    DispatchQueue.main.async {
+                        self.showEmptyStateView(with: message, in: self.view)
+                    }
+                    return
+                }
                 self.updateData()
                 
             case .failure(let error):
