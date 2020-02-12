780e7171da41dc7285a979e63b994d1b2975464f
jesseleeruiz <jesseleeruiz@gmail.com>
2020-02-12 10:27:55 -1000

    Added Secondary Title Label File.

---
+++ GitHubFollowers/GitHubFollowers/Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GitHubFollowers
+//
+//  Created by Jesse Ruiz on 2/12/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ GitHubFollowers/GitHubFollowers/Views/View Controllers/GFUserInfoHeaderViewController.swift
@@ -0,0 +1,17 @@
+//
+//  GFUserInfoHeaderViewController.swift
+//  GitHubFollowers
+//
+//  Created by Jesse Ruiz on 2/12/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class GFUserInfoHeaderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
